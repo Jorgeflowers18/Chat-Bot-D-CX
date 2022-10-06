f70f47ee87ab88d4e32ad91ac9490138a9a1db75
Jorgeflowers18 <jefo20156@gmail.com>
2022-10-06 17:00:50 -0500

    Chatbot_angularJs_dialogflowCX

---
+++ src/app/pages/entities/interfaces/entity.interface.ts
@@ -0,0 +1,10 @@
+export interface Entity{
+    id: number;
+    name: string;
+
+    "id": 1,
+        "name": "Park Row at Beekman St",
+        "address": "38 Park Row",
+        "city": "New York",
+        "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
+}
